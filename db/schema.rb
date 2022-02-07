7ad93a10879f82119b0885773e3e917aa3111d9a
Brett Gibson <brettgibson5@gmail.com>
2022-02-07 16:34:34 -0600

    change to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_02_07_053555) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "people", force: :cascade do |t|
     t.string "name"
     t.integer "rounds"
